4aaf32513636720d3572bbdfcb6c118eb27f27ef
Ravi Snellenberg <r.snellenberg@student.tudelft.nl>
2022-03-22 15:42:16 +0100

    Fix name

--- ACO-Python-master/src/AntColonyOptimization.py
+++ ACO-Python-master/src/AntColonyOptimization.py
@@ -39,7 +39,7 @@ class AntColonyOptimization:
                 routes.append(current)
                 if current_shortest_route is None or current.shorter_than(current_shortest_route):
                     current_shortest_route = current
-            self.maze.evaporate(evap)
+            self.maze.evaporate(self.evaporation)
             self.maze.add_pheromone_routes(routes, self.q)
         return current_shortest_route
 
